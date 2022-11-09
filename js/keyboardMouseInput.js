ebcc44a775792fe2061e34bffd0f4377c3911651
baek_seungyeon <rosalinebaek@gmail.com>
2022-11-08 19:09:56 -0800

    Updates

---
+++ js/keyboardMouseInput.js
@@ -0,0 +1,35 @@
+// document.addEventListener("keydown", function(event) {
+//     if (event.key === "Enter") {
+//         alert("enter key pressed");
+//     }
+//     if (event.key === " ") {
+//         alert("space key pressed");
+//         console.log("space pressed");
+//     }
+//     if (event.key === "W") {
+//         console.log("left key pressed");
+//     }
+// })
+
+// document.addEventListener("touchstart", function(event) {
+//     console.log("touched screen");
+//     shouldFlipCoin = !shouldFlipCoin;
+//     if (shouldFlipCoin === false) {
+//         let str = frogFortune[Math.floor(Math.random() * frogFortune.length)];
+//     }
+// })
+
+// document.addEventListener("keydown", function(event) {
+//     if (event.key === "w" || event.key === "W") {
+//         console.log("move up");
+//     }
+//     if (event.key === "a" || event.key === "A") {
+//         console.log("move left");
+//     }
+//     if (event.key === "d" || event.key === "D") {
+//         console.log("move right");
+//     }
+//     if (event.key === "s" || event.key === "S") {
+//         console.log("move down");
+//     }
+// })
---
+++ src/MainScene.js
@@ -0,0 +1,47 @@
+let gomPosition = new THREE.Vector2(0,0);
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "w" || event.key === "W") {
+        // console.log("move up");
+        gomPosition.y += 0.05;
+    }
+    if (event.key === "a" || event.key === "A") {
+        // console.log("move left");
+        gomPosition.x -= 0.05;
+    }
+    if (event.key === "d" || event.key === "D") {
+        // console.log("move right");
+        gomPosition.x += 0.05;
+    }
+    if (event.key === "s" || event.key === "S") {
+        // console.log("move down");
+        gomPosition.y -= 0.05;
+    }
+})
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// adding sprite render
+
+let gomTexture = new THREE.TextureLoader().load('../assets/gom.png');
+gomTexture.magFilter = THREE.NearestFilter;
+let gomMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, map: gomTexture, side: THREE.DoubleSide});
+const gomGeometry = new THREE.PlaneGeometry(1.2, 1.2);
+const gom = new THREE.Mesh( gomGeometry, gomMaterial);
+scene.add(gom);
+
+camera.position.z = 5;
+
+function animate() {
+    gom.position.set(gomPosition.x, gomPosition.y);
+    console.log(gom.position);
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+};
+
+animate();
