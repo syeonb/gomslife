c2b9d700c6fac5e6c1710fa2c16a7c98e527c965
baek_seungyeon <rosalinebaek@gmail.com>
2022-11-10 01:38:09 -0800

    Updates

--- src/MainScene.js
+++ src/MainScene.js
@@ -1,21 +1,55 @@
 let gomPosition = new THREE.Vector2(0,0);
 
+isFlippedSprite = false;
+moveDown = [12, 13, 12, 14]
+moveUp = [10, 11, 10, 4]
+moveRightLeft = [15, 8, 15, 9]
+spriteIndex = 0;
+movingUpDownLeftRight = 0;
+let currentTime = 0;
+let clock = new THREE.Clock();
+
 document.addEventListener("keydown", function(event) {
     if (event.key === "w" || event.key === "W") {
-        // console.log("move up");
+        movingUpDownLeftRight = 1;
+        if (isFlippedSprite) {
+            gom.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+            isFlippedSprite = false;
+        }
         gomPosition.y += 0.05;
     }
     if (event.key === "a" || event.key === "A") {
-        // console.log("move left");
+        movingUpDownLeftRight = 3;
+        if (isFlippedSprite) {
+            gom.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+            isFlippedSprite = false;
+        }
         gomPosition.x -= 0.05;
     }
     if (event.key === "d" || event.key === "D") {
+        movingUpDownLeftRight = 4;
         // console.log("move right");
+        if (!isFlippedSprite) {
+            gom.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+            isFlippedSprite = true;
+        }
         gomPosition.x += 0.05;
     }
     if (event.key === "s" || event.key === "S") {
-        // console.log("move down");
+        movingUpDownLeftRight = 2;
+        if (isFlippedSprite) {
+            gom.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1));
+            isFlippedSprite = false;
+        }
         gomPosition.y -= 0.05;
+    }
+})
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === "s" || event.key === "S" || event.key === "a" || event.key ==="A"
+    || event.key == "w" || event.key === "W" || event.key === "d" || event.key === "D") {
+        movingUpDownLeftRight = 0;
+        spriteIndex = 0;
     }
 })
 
@@ -28,18 +62,57 @@ document.body.appendChild( renderer.domElement );
 
 // adding sprite render
 
-let gomTexture = new THREE.TextureLoader().load("assets/gom.png");
+let gomTexture = new THREE.TextureLoader().load("assets/gom spritesheet.png");
 gomTexture.magFilter = THREE.NearestFilter;
+gomTexture.wrapS = gomTexture.wrapT = THREE.RepeatWrapping; 
+gomTexture.repeat.set( 1/4, 1/4);
+SelectSprite(12);
 let gomMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff, map: gomTexture, side: THREE.DoubleSide});
-const gomGeometry = new THREE.PlaneGeometry(1.2, 1.2);
+const gomGeometry = new THREE.PlaneGeometry(2, 2);
 const gom = new THREE.Mesh( gomGeometry, gomMaterial);
 scene.add(gom);
 
 camera.position.z = 5;
 
+
+function SelectSprite(index) 
+{
+    let x = index % 4;
+    let y = Math.floor(index / 4); 
+    gomTexture.offset.x = x/4;
+    gomTexture.offset.y = y/4;
+}
+
 function animate() {
     gom.position.set(gomPosition.x, gomPosition.y);
-    console.log(gom.position);
+    let delta = clock.getDelta(); 
+    currentTime -= delta * 1000;
+    if (currentTime < 0) {
+        if (movingUpDownLeftRight == 2) {
+            // console.log("is moving down");
+            SelectSprite(moveDown[spriteIndex]);
+            spriteIndex++;
+            console.log(spriteIndex);
+            if (spriteIndex == 4) {
+                spriteIndex = 0;
+            }
+        } else if (movingUpDownLeftRight == 1) {
+            SelectSprite(moveUp[spriteIndex]);
+            spriteIndex++;
+            console.log(spriteIndex);
+            if (spriteIndex == 4) {
+                spriteIndex = 0;
+            }
+        } else if (movingUpDownLeftRight == 3 || movingUpDownLeftRight == 4) {
+            SelectSprite(moveRightLeft[spriteIndex]);
+            spriteIndex++;
+            console.log(spriteIndex);
+            if (spriteIndex == 4) {
+                spriteIndex = 0;
+            }
+        }
+        currentTime = 500;
+    }
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 };
